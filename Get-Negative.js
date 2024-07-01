502f50fc0360ac179377f9019317b92119a755a2
ibrahimratrout <ibrahimratrout11@gmail.com>
2024-07-01 10:47:24 +0300

    Fifth problem

---
+++ Get-Negative.js
@@ -0,0 +1,12 @@
+//Get nagative value  ,but negative stay negative
+
+function getNegative(num){
+    if(num>0){
+    return -num
+    }else{
+        return num
+    }
+}
+
+console.log(getNegative(230))
+console.log(getNegative(-40))
