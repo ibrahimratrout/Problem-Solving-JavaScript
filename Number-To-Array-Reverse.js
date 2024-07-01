48815fb8e4ffb2a4400063529cd5f0454d115a0d
ibrahimratrout <ibrahimratrout11@gmail.com>
2024-07-01 11:27:35 +0300

    Edit

--- Number-To-Array-Reverse.js
+++ Number-To-Array-Reverse.js
@@ -1,9 +1,12 @@
-//Any number given return it with reverse array
+//Any number given return it with array then reverse it
 
 
 function digitRev(num){
     let newNum=String(num).split("").map(m=>Number(m)).reverse()
     console.log(newNum)
 }
+
+//some senarios 
+
 digitRev(123456789)
 digitRev(987654321)
