394e05384cea2c91b99b355c94646df6ae0338c4
ibrahimratrout <ibrahimratrout11@gmail.com>
2024-07-10 13:00:37 +0300

    Problem solving

---
+++ Remove-Duplicates.js
@@ -0,0 +1,18 @@
+//Remove duplicates from given array
+
+// function removeDup(arr){
+//     let finalArr=[]
+//     for(let i=0;i<arr.length;i++){
+//         if(!finalArr.includes(arr[i])){
+//             finalArr.push(arr[i])
+//         }
+//     }
+//     return finalArr
+// }
+
+function removeDup(arr){
+    const set=new Set(arr)
+    return set
+}
+
+console.log(removeDup([1,1,2,4,5,6,6,6,6,2]))
